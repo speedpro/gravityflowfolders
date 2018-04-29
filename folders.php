4b1b98b6b1d34c02e191b8f8f5f8828a46eb01f5
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-04-29 10:22:23 +0100

    Updated copyright

--- folders.php
+++ folders.php
@@ -9,7 +9,7 @@ Author URI: https://gravityflow.io
 License: GPL-3.0+
 
 ------------------------------------------------------------------------
-Copyright 2015-2016 Steven Henty
+Copyright 2015-2018 Steven Henty S.L.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
